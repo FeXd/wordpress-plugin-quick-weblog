73fe5102468223cdb6d2660910ae2f4dca6779b5
Arlin Schaffel <arlin@fexd.com>
2023-04-06 10:19:00 -0600

    Refactor getArticle to fetchAndPopulateFormFields
    - makes a bit more sense split out

--- quick-weblog.js
+++ quick-weblog.js
@@ -1,16 +1,5 @@
-function getArticle(url, api_key) {
-
-  if (url === "") {
-    updateStatus("You  must fill out <strong>Post URL</strong> to use Auto Fill.");
-    return;
-  } else {
-    updateStatus("Attempting to fetch information and Auto Fill...");
-  }
-
-  disableForm();
-
+function fetchArticleData(url, api_key) {
   const base_url = 'https://article-extractor2.p.rapidapi.com/article/parse?url=';
-
   const options = {
     method: 'GET',
     headers: {
@@ -19,37 +8,55 @@ function getArticle(url, api_key) {
     }
   };
 
-  fetch(base_url + url, options)
-	.then(response => {
-    if (response.status !== 200) {
-      throw new Error(`Request failed with status: ${response.status}`);
-    }
-    return response.json();
-  })
-	.then(response => {
-    console.log(response);
+  return fetch(base_url + url, options)
+    .then(response => {
+      if (response.status !== 200) {
+        throw new Error(`Request failed with status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(response => {
+      if (response.error > 0) {
+        throw new Error(`Request received error: ${response.message}`);
+      }
+      return response.data;
+    })
+    .catch(err => {
+      throw new Error(`Error fetching article data: ${err.message}`);
+    });
+}
 
-    if (response.error > 0) {
-      throw new Error(`Request received error: ${response.message}`);
-    }
+function updateFormFields(data) {
+  document.getElementById("quick-weblog-title").value = data?.title;
+  document.getElementById("quick-weblog-image_url").value = data?.image;
+  document.getElementById("quick-weblog-image_description").value = "image via " + data?.source;
+  document.getElementById("quick-weblog-quote").value = data?.description;
+}
 
-    document.getElementById("quick-weblog-title").value = response.data?.title;
-    document.getElementById("quick-weblog-image_url").value = response.data?.image;
-    document.getElementById("quick-weblog-image_description").value = "image via " + response.data?.source;
-    document.getElementById("quick-weblog-quote").value = response.data?.description;
+function fetchAndPopulateFormFields(articleUrl, rapidApiKey) {
+  if (articleUrl === "") {
+    updateStatus("You must fill out <strong>Post URL</strong> to use Auto Fill.");
+    return;
+  }
 
-    updateStatus("Successfully used Auto Fill to populate form!");
-    enableForm();
-  })
-	.catch(err => {
-    updateStatus(err);
-    console.error(err);
-    enableForm();
-  });
+  updateStatus("Attempting to fetch information and Auto Fill...");
+  disableForm();
+
+  fetchArticleData(articleUrl, rapidApiKey)
+    .then(data => {
+      updateFormFields(data);
+      updateStatus("Successfully used Auto Fill to populate form!");
+      enableForm();
+    })
+    .catch(err => {
+      updateStatus(err);
+      console.error(err);
+      enableForm();
+    });
 }
 
 function toggleFormElements(enable) {
-  const form= document.getElementById("quick-weblog");
+  const form = document.getElementById("quick-weblog");
   const formElements = form.querySelectorAll("input, select, textarea");
 
   formElements.forEach(element => {
